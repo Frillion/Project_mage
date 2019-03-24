43cc78b6cbe54ec24446ffbbb040e1934c8f0572
Frillion <fridriksoebech@gmail.com>
2019-03-24 16:47:17 +0000

    Finally Done With This Project, It Was A Good Learning Experience

---
+++ Assets/Scripts/main menu/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadbyIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
